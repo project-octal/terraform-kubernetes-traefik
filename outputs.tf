4b93886199ab2b8957263ea8ba9af1ee65f70543
Dylan Turnbull <dylanturn@gmail.com>
2021-03-05 08:51:14 -0600

    test

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-output "ingress_loadbalancer_ip_list" {
-  value = kubernetes_service.service.load_balancer_ingress
-}
+#output "ingress_loadbalancer_ip_list" {
+#  value = kubernetes_service.service.load_balancer_ingress
+#}
 output "ingress_class" {
   value = local.ingress_class
 }
