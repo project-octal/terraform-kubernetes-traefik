1eef22899ae7eb597daf2c19fe85ba4de9b6faa8
Dylan Turnbull <dylanturn@gmail.com>
2021-03-29 17:49:53 -0500

    added a long depends on in the output to avoid race conditions

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,14 @@
-#output "ingress_loadbalancer_ip_list" {
-#  value = kubernetes_service.service.load_balancer_ingress
-#}
 output "ingress_class" {
+  # Make sure we're avoiding any race conditions here.
+  depends_on = [
+    kubernetes_deployment.deployment,
+    k8s_manifest.traefik_services,
+    k8s_manifest.tls_stores,
+    k8s_manifest.tls_options,
+    k8s_manifest.middlewares,
+    k8s_manifest.ingress_route_udp,
+    k8s_manifest.ingress_route_tcp,
+    k8s_manifest.ingress_route
+  ]
   value = local.ingress_class
 }
