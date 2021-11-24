12bc6c8f57d7d5b29cdd67f48166a44a7e38c174
Dylan Turnbull <dylanturn@gmail.com>
2021-11-24 17:42:47 -0600

    removed the k8s_manifest dependency

--- versions.tf
+++ versions.tf
@@ -1,13 +1,9 @@
 terraform {
-  required_version = ">= 0.14, < 2.0.0"
+  required_version = ">= 0.14.8, < 2.0.0"
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.6.1"
-    }
-    k8s = {
-      source  = "banzaicloud/k8s"
-      version = "0.8.0"
     }
   }
   experiments = [module_variable_optional_attrs]
